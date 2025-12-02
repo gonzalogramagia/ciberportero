0c6b71fd7c9baa8dc6f5230d9e3677af39a7613c
Gonza <gonzalogramagia@gmail.com>
2025-12-02 12:53:53 -0300

    Agregar las principales responsabilidades de mis laburos

--- app/footer.tsx
+++ app/footer.tsx
@@ -64,7 +64,7 @@ export function Footer() {
   return (
     <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
       <div className="flex items-center justify-between">
-        <a href="https://github.com/ibelick/nim" target="_blank">
+        <a href="https://catsulecorp.com/" target="_blank">
           <span className="text-xs text-zinc-500">© 2025 Catsule Corp</span>
         </a>
         <div className="text-xs text-zinc-400">
