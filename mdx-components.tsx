f1282114bd0d1df5f2531b6b114286890c639d60
Gonza <gonzalogramagia@gmail.com>
2025-12-05 11:46:59 -0300

    Agregar zoom a las imagenes de la experiencia laboral

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,11 +2,13 @@ import type { MDXComponents } from 'mdx/types'
 import { ComponentPropsWithoutRef } from 'react'
 import { highlight } from 'sugar-high'
 import { MdxCover } from '@/components/mdx-cover'
+import { ZoomImage } from '@/components/zoom-image'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
     Cover: MdxCover,
+    img: ZoomImage as any,
     code: ({ children, ...props }: ComponentPropsWithoutRef<'code'>) => {
       const codeHTML = highlight(children as string)
       return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
